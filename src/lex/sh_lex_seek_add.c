e56b5329d669e06ff8ab1b910ac217790487a556
xbluecode <besbes.af@gmail.com>
2019-05-26 19:06:38 +0300

    Fixing Makefile & _lex_seek_add.c

--- src/lex/sh_lex_seek_add.c
+++ src/lex/sh_lex_seek_add.c
@@ -7,16 +7,19 @@
 
 int		sh_lex_seek_add(t_lex *lex, ssize_t off)
 {
-	int l;
+	int		l;
+	t_token	*tok;
 
 	if (lex->st == TSNONE)
 		return (0);
 	l = lex->i - off;
 	if (lex->st == TSW)
-	{
-		sh_lex_tok_add(lex, sh_lex_tok_new(lex->st, off, l,
-			ft_strndup(lex->in->str + off, l)));
-	}
+		tok = sh_lex_tok_new(lex->st, off, l,
+			ft_strndup(lex->in->str + off, l));
 	else
-		sh_lex_tok_add(lex, sh_lex_tok_new(lex->st, off, l, NULL));
+		tok = sh_lex_tok_new(lex->st, off, l, NULL);
+	sh_lex_tok_add(lex, tok);
+	lex->st = TSNONE;
+	ft_printf("Tok: %-2d - %s", tok->t, tok->val->str);
+	return (1);
 }
