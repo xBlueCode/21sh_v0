c56b4eb8daf3826725ac349b67063099e17c1573
xbluecode <besbes.af@gmail.com>
2019-05-28 23:40:39 +0300

    Adding __lex_seek_bq

--- src/lex/sh_lex_seek_q.c
+++ src/lex/sh_lex_seek_q.c
@@ -20,3 +20,19 @@ int 			sh_lex_seek_sq(t_lex *lex, int op)
 	lex->st = TSSQ;
 	return (1);
 }
+
+int 			sh_lex_seek_bq(t_lex *lex, int op)
+{
+	if (lex->in->str[lex->i] != '`')
+		return (0);
+	while (lex->in->str[lex->i] && lex->in->str[++lex->i] != '`')
+		;
+	if (!lex->in->str[lex->i])
+	{
+		lex->err = 1;
+		return (-1);
+	}
+	lex->i++;
+	lex->st = TSSQ;
+	return (1);
+}
