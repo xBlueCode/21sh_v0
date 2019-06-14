dd27f969961a569b7642771db6a2eb4bf2b9acb2
xbluecode <besbes.af@gmail.com>
2019-06-14 20:34:49 +0300

    Implementing sh_lex_print

--- src/lex/sh_lex_print.c
+++ src/lex/sh_lex_print.c
@@ -2,3 +2,29 @@
 // Created by xbluecode on 28.05.19.
 //
 
+#include "libft.h"
+#include "sh_lex.h"
+
+int				sh_lex_print(t_lex *lex)
+{
+	t_list 	*lstHead;
+	t_token	*token;
+	int		i;
+
+	if (!lex)
+		ft_printf("Invalid Lexer !\n");
+	else if (!(lstHead = lex->tlst))
+		ft_printf("No token found !");
+	else
+	{
+		i = 0;
+		while (lstHead)
+		{
+			if ((token = (t_token*)lstHead->content))
+				ft_printf("-%d\tT: <%-02d>\tV: <%s>",
+					i, token->t, token->val);
+			lstHead = lstHead->next;
+		}
+	}
+	return (0);
+}
