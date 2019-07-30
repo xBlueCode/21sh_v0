a7613ea631aa1d65bc09aad4996d0ec21a8ec87c
xbluecode <besbes.af@gmail.com>
2019-07-30 08:22:25 +0300

    Implementing RL scoping in Lexer for: param, scmd, smath

--- src/lex/sh_lex_seek_s.c
+++ src/lex/sh_lex_seek_s.c
@@ -9,10 +9,12 @@ int 			sh_lex_seek_param(t_lex *lex, int op)
 {
 	if (ft_strncmp(lex->in->str + lex->i, "${", 2))
 		return (0);
+	ft_dstrins_ch(lex->scope, -1, RL_SCP_PARAM);
 	lex->i += 2;
 	while (lex->in->str[lex->i] && lex->in->str[lex->i] != '}')
 	{
-		if (sh_lex_seek_join(lex, op)
+		if (sh_lex_seek_rescope(lex, op)
+			|| sh_lex_seek_join(lex, op)
 			//|| sh_lex_seek_space(lex, op)
 			|| sh_lex_seek_escape(lex, op)
 			//|| sh_lex_seek_sq(lex, op)
@@ -24,9 +26,11 @@ int 			sh_lex_seek_param(t_lex *lex, int op)
 		else
 			lex->i++;
 	}
-	if (!lex->in->str[lex->i])
-		SH_LEX_RETERR(lex, TSD_CBL)
-	lex->i++;
+	if (lex->in->str[lex->i] == '}' && ++lex->i)
+		ft_dstrdel_n(lex->scope, -1, 1);
+	//if (!lex->in->str[lex->i])
+	//	SH_LEX_RETERR(lex, TSD_CBL)
+	//lex->i++;
 	lex->st = TSD_CBL;
 	return (1);
 }
@@ -35,12 +39,15 @@ int 			sh_lex_seek_scmd(t_lex *lex, int op)
 {
 	if (ft_strncmp(lex->in->str + lex->i, "$(", 2))
 		return (0);
+	ft_dstrins_ch(lex->scope, -1, RL_SCP_SCMD);
 	lex->i += 2;
 	while (lex->in->str[lex->i] && lex->in->str[lex->i] != ')')
 	{
-		if (sh_lex_seek_join(lex, op)
+		if (sh_lex_seek_rescope(lex, op)
+			|| sh_lex_seek_join(lex, op)
 			//|| sh_lex_seek_space(lex, op)
 			|| sh_lex_seek_escape(lex, op)
+			|| sh_lex_seek_op(lex, op)
 			|| sh_lex_seek_sq(lex, op)
 			|| sh_lex_seek_dq(lex, op)
 			|| sh_lex_seek_bq(lex, op)
@@ -53,9 +60,11 @@ int 			sh_lex_seek_scmd(t_lex *lex, int op)
 		else
 			lex->i++;
 	}
-	if (!lex->in->str[lex->i])
-		SH_LEX_RETERR(lex, TSD_PL)
-	lex->i++;
+	if (lex->in->str[lex->i] == ')' && ++lex->i)
+		ft_dstrdel_n(lex->scope, -1, 1);
+	//if (!lex->in->str[lex->i])
+	//	SH_LEX_RETERR(lex, TSD_PL)
+	//lex->i++;
 	lex->st = TSD_PL;
 	return (1);
 }
@@ -64,10 +73,12 @@ int 			sh_lex_seek_smath(t_lex *lex, int op)
 {
 	if (ft_strncmp(lex->in->str + lex->i, "$((", 3))
 		return (0);
+	ft_dstrins_ch(lex->scope, -1, RL_SCP_SMATH);
 	lex->i += 3;
 	while (lex->in->str[lex->i] && ft_strncmp(lex->in->str + lex->i, "))", 2))
 	{
-		if (sh_lex_seek_join(lex, op)
+		if (sh_lex_seek_rescope(lex, op)
+			|| sh_lex_seek_join(lex, op)
 			//|| sh_lex_seek_space(lex, op)
 			|| sh_lex_seek_escape(lex, op)
 			//|| sh_lex_seek_sq(lex, op)
@@ -82,9 +93,13 @@ int 			sh_lex_seek_smath(t_lex *lex, int op)
 		else
 			lex->i++;
 	}
-	if (!lex->in->str[lex->i])
-		SH_LEX_RETERR(lex, TSD_PL2)
-	lex->i += 2;
+	//if (!lex->in->str[lex->i])
+	//	SH_LEX_RETERR(lex, TSD_PL2)
+	//if (lex->in->str[lex->i])
+	//	ft_dstrdel_n(lex->scope, -1, 1);
+	if (!ft_strncmp("))", lex->in->str + lex->i, 2) && (lex->i += 2))
+		ft_dstrdel_n(lex->scope, -1, 1);
+	//lex->i += 2;
 	lex->st = TSD_PL2;
 	return (1);
 }
