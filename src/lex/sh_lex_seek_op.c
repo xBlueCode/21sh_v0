9e82c1fbf693af9a5cf9536e01fe51c23b3c3b43
xbluecode <besbes.af@gmail.com>
2019-08-01 07:41:46 +0300

    Fixing RL Scoping for: &&

--- src/lex/sh_lex_seek_op.c
+++ src/lex/sh_lex_seek_op.c
@@ -25,7 +25,10 @@ int 	sh_lex_seek_op_sc(t_lex *lex, int op)
 		if (lex->in->str[++lex->i] == ';' && ++lex->i)
 			lex->st = TSSC2;
 		else
+		{
 			lex->st = TSSC;
+			lex->ctx = TCTX_FIRSTW;
+		}
 		return (1);
 	}
 	return (0);
@@ -37,9 +40,13 @@ int 	sh_lex_seek_op_a(t_lex *lex, int op)
 	if (lex->in->str[lex->i] == '&' && ++lex->i)
 	{
 		if (lex->in->str[lex->i] == '&' && ++lex->i)
+		{
 			lex->st = TSA2;
+			ft_dstrins_ch(lex->scope, -1, RL_SCP_AND);
+		}
 		else
 			lex->st = TSA;
+		lex->ctx = TCTX_FIRSTW;
 		return (1);
 	}
 	return (0);
@@ -60,6 +67,7 @@ int 	sh_lex_seek_op_o(t_lex *lex, int op)
             lex->st = TSO;
 			ft_dstrins_ch(lex->scope, -1, RL_SCP_PIPE);
         }
+		lex->ctx = TCTX_FIRSTW;
 		return (1);
 	}
 	return (0);
