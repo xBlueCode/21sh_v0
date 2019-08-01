430f17fe70eba024da8dbf1837e572cde1f2e715
xbluecode <besbes.af@gmail.com>
2019-08-01 09:18:27 +0300

    Implementing the Context Tracker for the lexer

---
+++ src/lex/sh_lex_seek_ctx.c
@@ -0,0 +1,17 @@
+#include "libft.h"
+#include "sh_lex.h"
+
+int 	sh_lex_seek_ctx(t_lex *lex, int op)
+{
+	(void)op;
+	if (lex->st == TSNONE
+		||	lex->st == TSSC
+		|| lex->st == TSA || lex->st == TSA2
+		|| lex->st == TSO || lex->st == TSO2
+		|| lex->st == TSPL || lex->st == TSNL
+		)
+		lex->ctx = TCTX_FIRSTW;
+	else
+		lex->ctx = TCTX_NONE;
+	return (0);
+}
