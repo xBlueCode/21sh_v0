d13d3a2b7d9e6eb4902229ea9170bc670c30ca5e
xbluecode <besbes.af@gmail.com>
2019-08-16 03:17:33 +0300

    Updating job control DS

--- includes/sh_exec.h
+++ includes/sh_exec.h
@@ -2,7 +2,7 @@
 # define SH_EXEC_H
 
 #include "sh_parser.h"
-
+/*
 int			sh_e_loop(void *gr);
 int			sh_e_for(void *gr);
 int			sh_e_com_cmds(void *gr);
@@ -14,5 +14,19 @@ int			sh_e_cmd_core(int t);
 int			sh_e_simp_cmd(void *gr);
 int			sh_e_comp_cmd(void *gr);
 int			sh_e_group(void *gr);
+*/
+
+
+int			sh_e_loop(t_jcon *jc, void *gr);
+int			sh_e_for(t_jcon *jc, void *gr);
+int			sh_e_com_cmds(t_jcon *jc, void *gr);
+int			sh_e_com_cmd(t_jcon *jc, void *gr);
+int			sh_e_and_or(t_jcon *jc, void *gr);
+int			sh_e_pipe(t_jcon *jc, void *gr);
+int			sh_e_cmd(t_jcon *jc, void *gr);
+int			sh_e_cmd_core(int t);
+int			sh_e_simp_cmd(t_jcon *jc, void *gr);
+int			sh_e_comp_cmd(t_jcon *jc, void *gr);
+int			sh_e_group(t_jcon *jc, void *gr);
 
 #endif
--- includes/sh_jobc.h
+++ includes/sh_jobc.h
@@ -3,9 +3,15 @@
 
 #include "ftsh.h"
 
+/**
+ * pid			:
+ * completed	: true if process has completed
+ * stopped		: true if process has stopped
+ * status		: reported status value
+ */
 typedef struct	s_process {
-	struct			s_process *next; /* next process in pipeline */ // --> and_or
-	char			**argv; /* for exec */
+	//struct			s_process *next; /* next process in pipeline */ // --> and_or
+	//char			**argv; /* for exec */
 	pid_t			pid; /* process ID */
 	char			completed; /* true if process has completed */
 	char			stopped; /* true if process has stopped */
@@ -13,19 +19,33 @@ typedef struct	s_process {
 }				t_process;
 
 /* A job is a pipeline of processes. */
+/**
+ * ind			: job index
+ * pgid			: process group ID (pgid)
+ * notified		: true if user told about stopped job
+ */
 typedef struct	s_job {
 	int				ind;
 	pid_t			pgid; /* process group ID */
 	char			notified; /* true if user told about stopped job */
 	t_termios		tmodes; /* saved terminal modes */
-	t_and_or		*andor_list;
+	t_list			process;
+//	t_and_or		*andor_list;
 	char 			bg;
 }				t_job;
 
+/**
+** cind			: current index
+** jobs			: list of jobs
+** cjob			: current job being processed
+** sh_pid		: shell PID
+** sh_termios	: terminal atts of the shell
+*/
 typedef struct	s_jcon
 {
 	int			cind;
 	t_list		*jobs;
+	t_job		*cjob;
 	pid_t 		sh_pid; // it can be moved to the shell dashboard
 	t_termios	sh_termios; // it can be moved to shell dashboard
 }				t_jcon;
