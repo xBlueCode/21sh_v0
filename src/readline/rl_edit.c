994d0e983715cf76a3d08b98b8894e39a696f44e
xbluecode <besbes.af@gmail.com>
2019-09-05 20:37:41 +0300

    Fixing readline in case of inserting chars in the middle

--- src/readline/rl_edit.c
+++ src/readline/rl_edit.c
@@ -14,6 +14,8 @@
 #include "ft_rl.h"
 #include "ftsh.h"
 
+#include <fcntl.h>
+
 extern t_rl		g_rl;
 
 int				rl_insert_nl(int c)
@@ -28,40 +30,40 @@ int				rl_insert_nl(int c)
 
 int				rl_insert_ch(int c)
 {
-	char	*line;
+	t_dstr	*line;
 
 	rl_hist_reset();
-	line = g_rl.txt->a[g_rl.cl]->str;
+	line = g_rl.txt->a[g_rl.cl];
 	if (c == '\n')
 		return (rl_insert_nl(c));
-	ft_dstrins_ch(g_rl.txt->a[g_rl.cl], g_rl.cc++, c);
+	ft_dstrins_ch(line, g_rl.cc++, c);
 	ft_putchar(c);
 	if (((g_rl.cc + g_rl.plen + 1) % g_rl.wc == 1))
 	{
 		RL_TPUTS("cr");
 		RL_TPUTS("do");
 	}
-	if (line[g_rl.cc])
+	if (line->str[g_rl.cc])
 	{
-		rl_putstr_wrap(line, g_rl.cc);
-		rl_cur_fromto(ft_strlenz(line), g_rl.cc);
+		rl_putstr_wrap(line->str, g_rl.cc);
+		rl_cur_fromto(ft_strlenz(line->str), g_rl.cc);
 	}
 	return (0);
 }
 
 int				rl_ctrl_del_ch(int c)
 {
-	char	*line;
+	t_dstr	*line;
 
 	(void)c;
-	line = g_rl.txt->a[g_rl.cl]->str;
+	line = g_rl.txt->a[g_rl.cl];
 	if (!g_rl.cc)
 		return (0);
 	rl_cur_fromto(g_rl.cc, g_rl.cc - 1);
-	rl_putnchar_wrap(' ', ft_strlenz(line + g_rl.cc - 1), g_rl.cc - 1);
-	rl_cur_fromto(ft_strlenz(line), g_rl.cc - 1);
+	rl_putnchar_wrap(' ', ft_strlenz(line->str + g_rl.cc - 1), g_rl.cc - 1);
+	rl_cur_fromto(ft_strlenz(line->str), g_rl.cc - 1);
 	ft_dstrdel_n(g_rl.txt->a[g_rl.cl], --g_rl.cc, 1);
-	rl_putstr_wrap(line, g_rl.cc);
-	rl_cur_fromto(ft_strlenz(line), g_rl.cc);
+	rl_putstr_wrap(line->str, g_rl.cc);
+	rl_cur_fromto(ft_strlenz(line->str), g_rl.cc);
 	return (0);
 }
